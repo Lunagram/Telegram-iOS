d3c368568a57303fc433c9c09b1572822d39a075
Rio <pahapck@coupang.com>
2019-05-06 14:48:01 +0900

    get api id hash

--- Telegram-iOS/BuildConfig.m
+++ Telegram-iOS/BuildConfig.m
@@ -12,6 +12,8 @@
 
 #import <MtProtoKitDynamic/MtProtoKitDynamic.h>
 
+@import Lunagram;
+
 static uint32_t funcSwap32(uint32_t input)
 {
     return OSSwapBigToHostInt32(input);
@@ -272,63 +274,10 @@ static MTPKCS * _Nullable checkSignature(const char *filename) {
 - (instancetype)init {
     self = [super init];
     if (self != nil) {
-        char buf[3];
-        buf[2] = '\0';
-        NSString *hex = @(APP_CONFIG_DATA);
-        assert(0 == [hex length] % 2);
-        unsigned char *bytes = malloc([hex length]/2);
-        unsigned char *bp = bytes;
-        for (CFIndex i = 0; i < [hex length]; i += 2) {
-            buf[0] = [hex characterAtIndex:i];
-            buf[1] = [hex characterAtIndex:i+1];
-            char *b2 = NULL;
-            *bp++ = strtol(buf, &b2, 16);
-            assert(b2 == buf + 2);
-        }
         
-        NSMutableData *data = [NSMutableData dataWithBytesNoCopy:bytes length:[hex length]/2 freeWhenDone:YES];
-        if ([data length] == 0) {
-            assert(false);
-        }
-        
-        const char *streamCode = "Cypher";
-        int keyLength = (int)strlen(streamCode);
-        int keyOffset = 0;
-        for (NSUInteger i = 0; i < data.length; i++) {
-            ((uint8_t *)data.mutableBytes)[i] ^= ((uint8_t *)streamCode)[keyOffset % keyLength];
-            keyOffset += 1;
-        }
-        
-        int offset = 0;
-        uint32_t header = 0;
-        [data getBytes:&header range:NSMakeRange(offset, 4)];
-        offset += 4;
-        if (header != 0xabcdef01U) {
-            assert(false);
-        }
-        
-        [data getBytes:&_apiId range:NSMakeRange(offset, 4)];
-        offset += 4;
-        
-        int32_t apiHashLength = 0;
-        [data getBytes:&apiHashLength range:NSMakeRange(offset, 4)];
-        offset += 4;
-        
-        if (apiHashLength > 0) {
-            _apiHash = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(offset, apiHashLength)] encoding:NSUTF8StringEncoding];
-            offset += apiHashLength;
-        } else {
-            assert(false);
-        }
-        
-        int32_t hockeyappIdLength = 0;
-        [data getBytes:&hockeyappIdLength range:NSMakeRange(offset, 4)];
-        offset += 4;
-        
-        if (hockeyappIdLength > 0) {
-            _hockeyAppId = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(offset, hockeyappIdLength)] encoding:NSUTF8StringEncoding];
-            offset += hockeyappIdLength;
-        }
+        _apiId = [[ApiInfoUtil getApiInfoWithInfoKey:ApiInfoKeyApiId] integerValue];
+        _apiHash = [ApiInfoUtil getApiInfoWithInfoKey:ApiInfoKeyApiHash];
+        _hockeyAppId = nil;
         
         NSString *bundleId = [BuildConfig bundleId];
         
